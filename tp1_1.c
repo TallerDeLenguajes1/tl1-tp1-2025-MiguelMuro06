f100bb73c9b7cb0e1231ff3a3a418125c6e1d56e
Miguel Muro <muromiguel17@gmail.com>
2025-04-01 04:57:40 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main (){
+    int x= 4;
+    int *punt;
+    punt = &x;
 
-    printf("Hola mundo");
+    printf("\n Hola mundo");
+
+    printf("\n el contenido del puntero: %d", *punt);
+    
+    printf("\n La dirección de memoria almacenada por el puntero: %p", punt);
+
+    printf("\n La dirección de memoria de la variable: %p", &(*punt));
+
+    printf("\n La dirección de memoria del puntero: %p", &punt);
+
+    printf("\n el tamaño de memoria utilizado por esa variable usando la función sizeof(): %zu bytes\n", sizeof(*punt));
+
+    
+    
     return 0;
 }
